88334810ee2646df36b60147b6caaa5afeb416b8
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2022-01-12 16:03:32 +0300

    Rescaling images

--- Exercise1/main.cpp
+++ Exercise1/main.cpp
@@ -3,7 +3,6 @@
  The original copy of the code can be found at http://web.eecs.utk.edu/~mkarakay/courses_files/testfiles.zip
  and it is modified for ELM463/667
  **************************************/
-
 #include "Image.h"
 
 using namespace std;
@@ -19,21 +18,21 @@ int main(int argc, char** argv)
     /*Problem 1 Burada Bitiyor*/
     
 
-    /*Problem 2 Görüntüyü Yariya indirme burada basliyor*/
+    /*Problem 2 GÃ¶rÃ¼ntÃ¼yÃ¼ Yariya indirme burada basliyor*/
     Image input2, output2;
     char inputName[] = "Test_image.pgm";
     char outputName1[] = "Out2.pgm";
     input2.readImage(inputName);
     output2 = input2.RescaleSize(input2, 0.5);
     output2.writeImage(outputName1);
-    /*Problem 2 Görüntüyü Yariya indirme Burada bitiyor*/
+    /*Problem 2 GÃ¶rÃ¼ntÃ¼yÃ¼ Yariya indirme Burada bitiyor*/
 
 
-    /*Problem 2 Görüntüyü 2 kati büyütme burada basliyor*/
+    /*Problem 2 GÃ¶rÃ¼ntÃ¼yÃ¼ 2 kati bÃ¼yÃ¼tme burada basliyor*/
     Image input3, output3;
     char outputName2[] = "Out3.pgm";
     input3.readImage(inputName);
     output3 = input3.RescaleSize(input3, 2);
     output3.writeImage(outputName2);
-    /*Problem 2 Görüntüyü 2 kati büyütme Burada bitiyor*/
+    /*Problem 2 GÃ¶rÃ¼ntÃ¼yÃ¼ 2 kati bÃ¼yÃ¼tme Burada bitiyor*/
 }
